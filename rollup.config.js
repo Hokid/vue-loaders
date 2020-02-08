95af3ef7d1d858fba59f297a5a48acc1b6f81c28
Kirill Khoroshilov <mr.hokid@gmail.com>
2020-02-08 17:37:38 +0300

    chore: update pkg deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 import postcss from 'rollup-plugin-postcss';
